4ebd747744f219bcde448926bec967bd050a2773
Olga Yurchuk <olga.yourchooke@gmail.com>
2023-03-20 16:22:30 +0700

    Initial Commit

---
+++ TradeByBataApp/TradeByBataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeByBataAppApp.swift
+//  TradeByBataApp
+//
+//  Created by Olga Yurchuk on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TradeByBataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
