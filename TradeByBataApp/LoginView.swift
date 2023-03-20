a5131ed2a3429ee183e32e114c7cbf6ca2d217ac
Olga Yurchuk <olga.yourchooke@gmail.com>
2023-03-20 16:53:03 +0700

    Login and sign in views

---
+++ TradeByBataApp/LoginView.swift
@@ -0,0 +1,52 @@
+//
+//  Login.swift
+//  TradeByBata
+//
+//  Created by Olga Yurchuk on 14.03.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var name = ""
+    @State private var password = ""
+    
+    var body: some View {
+        VStack{
+            Text("Welcome back")
+                .fontWeight(.semibold)
+                .fontSize(size: 30)
+                
+            
+            GrayTextField(title: "First name", text: $name, isSecured: false)
+                .padding(.init(top: 60, leading: 0, bottom: 35, trailing: 0))
+            
+            GrayTextField(title: "Password", text: $password, isSecured: true)
+            
+            
+            Spacer(minLength: 99)
+            Button(action: {}) {
+                Spacer()
+                Text("Login")
+                    .font(AppFont().body)
+                    .bold()
+                Spacer()
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(Color(red: 0.306, green: 0.333, blue: 0.843))
+            
+            
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 140, leading: 43, bottom: 0, trailing: 43))
+        .ignoresSafeArea()
+        
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ TradeByBataApp/SignInView.swift
@@ -0,0 +1,76 @@
+//
+//  SignInView.swift
+//  TradeByBata
+//
+//  Created by Olga Yurchuk on 17.03.2023.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @State private var name = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    
+    var body: some View {
+        VStack{
+            Text("Sign in")
+                .fontWeight(.semibold)
+                .fontSize(size: 30)
+            
+            GrayTextField(title: "First name", text: $name, isSecured: false)
+                .padding(.init(top: 60, leading: 0, bottom: 0, trailing: 0))
+            GrayTextField(title: "Last name", text: $lastName)
+                .padding(.init(top: 35, leading: 0, bottom: 0, trailing: 0))
+            GrayTextField(title: "Email", text: $email)
+                .padding(.init(top: 35, leading: 0, bottom: 0, trailing: 0))
+
+            Button(action: {}) {
+                Spacer()
+                Text("Login")
+                    .font(AppFont().body)
+                    .bold()
+                Spacer()
+            } .frame(height: 46)
+            .buttonStyle(.borderedProminent)
+            .tint(Color(red: 0.306, green: 0.333, blue: 0.843))
+            .padding(.init(top: 35, leading: 0, bottom: 0, trailing: 0))
+            
+            HStack {
+                Text("Already have an account?")
+                NavigationLink("Log in", destination: LoginView())
+                    .foregroundColor(.blue)
+                Spacer()
+            }.font(AppFont().footnote)
+            
+            VStack{
+                HStack{
+                    Image("GooglePic")
+                        .frame(width: 25, height: 25)
+                    Text("Sign in with Google")
+                    Spacer()
+                }
+                HStack{
+                    Image("ApplePic")
+                        .scaleEffect(1.23)
+                        .frame(width: 25, height: 25)
+                    Text("Sign in with Apple")
+                    Spacer()
+                }
+            }
+                .font(AppFont().body)
+                .padding(.init(top: 35, leading: 40, bottom: 0, trailing: 40))
+            
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 140, leading: 43, bottom: 0, trailing: 43))
+        .ignoresSafeArea()
+        
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
