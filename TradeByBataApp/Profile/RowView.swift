8e58e5de879e393ba965da6db9b21700e17fdbbc
Olga Yurchuk <olga.yourchooke@gmail.com>
2023-03-20 20:22:32 +0700

    Row view in profile

---
+++ TradeByBataApp/Profile/RowView.swift
@@ -0,0 +1,44 @@
+//
+//  RowView.swift
+//  TradeByBataApp
+//
+//  Created by Olga Yurchuk on 20.03.2023.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    let viewModel: ProfileViewModel
+    let imageName: String
+    let text: String
+    let value: String?
+    
+    var body: some View {
+        HStack{
+            Image(systemName: imageName)
+                .scaleEffect(1.4)
+                .frame(width: 50, height: 50)
+                .background(Color(red: 0.933, green: 0.937, blue: 0.957))
+                .clipShape(Circle())
+            Text(text)
+            if let value = value {
+                Spacer()
+                Text(value)
+            }
+        }
+            .font(AppFont().body)
+    }
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowView(
+            viewModel: ProfileViewModel(
+                user: User.getUsers()),
+            imageName: "arrow.triangle.2.circlepath",
+            text: "Restore purchase",
+            value: "111"
+        )
+        .padding()
+    }
+}
