00580c36ea53751976f1dcc24c40ffe131b8560a
Olga Yurchuk <olga.yourchooke@gmail.com>
2023-03-20 16:44:08 +0700

    User model

---
+++ TradeByBataApp/Model/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  TradeByBata
+//
+//  Created by Olga Yurchuk on 19.03.2023.
+//
+
+import Foundation
+
+struct User: Decodable {
+    var firstName: String
+    var lastName: String
+    var email: String
+    var photoUrl: String
+    var password: String?
+    var balance: Double?
+    
+    var fullname: String {
+        "\(firstName)  \(lastName)"
+    }
+    
+    static func getUsers() -> User {
+        return User(
+            firstName: "Sadhi",
+            lastName: "Adhi Pradana",
+            email: "sadhi@mail.ru",
+            photoUrl: "https://github.com/yourchooke/TradeByBata/blob/main/Mocks/avatar.png?raw=true",
+            password: "123",
+            balance: 1593
+        )
+        
+    }
+}
+
