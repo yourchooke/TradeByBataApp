0562b82433b51bc90afd7abbaa86fc2e582b47f5
Olga Yurchuk <olga.yourchooke@gmail.com>
2023-03-22 14:53:33 +0700

    Home view model is created

---
+++ TradeByBataApp/Home/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  TradeByBataApp
+//
+//  Created by Olga Yurchuk on 22.03.2023.
+//
+
+import Foundation
+
--- TradeByBataApp/Services/NetworkManager.swift
+++ TradeByBataApp/Services/NetworkManager.swift
@@ -15,23 +15,18 @@ class NetworkManager {
     
     
     func fetchImageData(from url: String) -> Data? {
-        guard let imageURL = URL(string: url) else {return nil}
+        guard let imageURL = URL(string: url) else { return nil }
         guard let imageData = try? Data(contentsOf: imageURL) else { return nil }
         return imageData
     }
     
     func fetchLatestData(from url: String) async -> Void {
         guard let url = URL(string: url) else { return }
-    //    print(url)
-    //    let decoder = JSONDecoder()
-    //    decoder.keyDecodingStrategy = .convertFromSnakeCase
         
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            print(data.first)
             let decoderResponse = try JSONDecoder().decode(Latest.self, from: data)
             
-            print(decoderResponse)
         } catch {
             print("data isn't valid")
         }
@@ -42,15 +37,4 @@ class NetworkManager {
 
 enum Constants: String {
     case latestURL = "https://run.mocky.io/v3/cc0071a1-f06e-48fa-9e90-b1c2a61eaca7"
-}
-
-
-public struct Throwable<T: Decodable>: Decodable {
-    
-    public let result: Result<T, Error>
-
-    public init(from decoder: Decoder) throws {
-        let catching = { try T(from: decoder) }
-        result = Result(catching: catching )
-    }
 }
